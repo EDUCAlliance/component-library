8d1bae8c21c33027fa3e9b8b2f648dd7ea157d4f
Lukáš Pánek <lukaspanek5@gmail.com>
2025-04-16 14:39:46 +0200

    feat: setup storybook

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,27 @@
 import { fileURLToPath } from 'node:url'
+import path from 'node:path'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
+import { storybookTest } from '@storybook/experimental-addon-test/vitest-plugin'
+
+const dirname =
+  typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url))
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
+    plugins: [storybookTest({ configDir: path.join(dirname, '.storybook') })],
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
+      setupFiles: ['.storybook/vitest.setup.ts'],
+      browser: {
+        enabled: true,
+        headless: true,
+        provider: 'playwright',
+        instances: [{ browser: 'chromium' }],
+      },
     },
   }),
 )
