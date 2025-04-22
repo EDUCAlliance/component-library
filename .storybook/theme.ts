04a992e0183914ec5af8a548f62f8b2dca407d98
Lukáš Pánek <lukaspanek5@gmail.com>
2025-04-22 13:29:48 +0200

    fix:logo on imgur

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -1,32 +1,32 @@
-import { create } from '@storybook/theming'
+import { create } from "@storybook/theming"
 
 export default create({
-  base: 'light',
-  brandTitle: `<img src="http://localhost:5173/logo.png" height="64px"/>`,
-  brandUrl: 'https://www.educalliance.eu/',
-  brandTarget: '_self',
+  base: "light",
+  brandTitle: `<img src="https://i.imgur.com/VdPmK4G.png" height="64px"/>`,
+  brandUrl: "https://www.educalliance.eu/",
+  brandTarget: "_self",
 
   fontBase: '"Inter", sans-serif',
-  fontCode: 'monospace',
+  fontCode: "monospace",
 
-  colorPrimary: 'rgb(40, 190, 127)',
-  colorSecondary: 'rgb(40, 190, 127)',
+  colorPrimary: "rgb(40, 190, 127)",
+  colorSecondary: "rgb(40, 190, 127)",
 
   // UI
-  appBg: '#ffffff',
-  appContentBg: '#ffffff',
-  appPreviewBg: '#ffffff',
+  appBg: "#ffffff",
+  appContentBg: "#ffffff",
+  appPreviewBg: "#ffffff",
   appBorderRadius: 8,
 
   // Text colors
-  textColor: '#10162F',
-  textInverseColor: '#ffffff',
+  textColor: "#10162F",
+  textInverseColor: "#ffffff",
 
   // Toolbar default and active colors
-  barTextColor: 'rgb(61, 61, 59)',
-  barSelectedColor: 'rgb(40, 190, 127)',
-  barHoverColor: 'rgb(40, 190, 127)',
-  barBg: '#ffffff',
+  barTextColor: "rgb(61, 61, 59)",
+  barSelectedColor: "rgb(40, 190, 127)",
+  barHoverColor: "rgb(40, 190, 127)",
+  barBg: "#ffffff",
 
   /*// Form colors
   inputBg: '#ffffff',
