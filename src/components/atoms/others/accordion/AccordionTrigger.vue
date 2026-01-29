13cb5dbdf6a3a3db0f9b8c74e77815b2086b3221
Lukáš Pánek <lukaspanek5@gmail.com>
2026-01-29 10:02:33 +0100

    feat: replace font awesome in accordion

--- src/components/atoms/others/accordion/AccordionTrigger.vue
+++ src/components/atoms/others/accordion/AccordionTrigger.vue
@@ -29,25 +29,36 @@ const size = inject<"sm" | "md" | "lg">("accordionSize", "md")
       <slot />
       <slot name="icon">
         <div class="relative h-5 w-5">
-          <font-awesome-icon
-            :icon="['fas', 'plus']"
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 640 640"
+            data-icon="plus"
             :class="
               cn(
-                size === 'lg' ? 'text-2xl' : size === 'md' ? 'text-lg' : 'text-base',
+                size === 'lg' ? 'size-6' : size === 'md' ? 'size-6' : 'size-4',
                 'text-black-primary pointer-events-none absolute top-1/2 right-0 shrink-0 -translate-y-1/2 duration-200',
               )
             "
-          />
-
-          <font-awesome-icon
-            :icon="['fas', 'minus']"
+          >
+            <path
+              d="M352 128C352 110.3 337.7 96 320 96C302.3 96 288 110.3 288 128L288 288L128 288C110.3 288 96 302.3 96 320C96 337.7 110.3 352 128 352L288 352L288 512C288 529.7 302.3 544 320 544C337.7 544 352 529.7 352 512L352 352L512 352C529.7 352 544 337.7 544 320C544 302.3 529.7 288 512 288L352 288L352 128z"
+            />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 640 640"
+            data-icon="minus"
             :class="
               cn(
-                size === 'lg' ? 'text-2xl' : size === 'md' ? 'text-lg' : 'text-base',
+                size === 'lg' ? 'size-6' : size === 'md' ? 'size-6' : 'size-4',
                 'text-black-primary pointer-events-none absolute top-1/2 right-0 shrink-0 -translate-y-1/2 duration-200',
               )
             "
-          />
+          >
+            <path
+              d="M96 320C96 302.3 110.3 288 128 288L512 288C529.7 288 544 302.3 544 320C544 337.7 529.7 352 512 352L128 352C110.3 352 96 337.7 96 320z"
+            />
+          </svg>
         </div>
       </slot>
     </AccordionTrigger>
